9c69b4b2bb0e3bc45c1252297740d617105dc982
JapaneseZgredek <fabian.zbranski@gmail.com>
2024-07-01 20:45:04 +0200

    Change URL, get titles
    Previous URL was IP secured, so I decided to change URL to another site with movie ratings

--- Web_scraping_IMBD/main.py
+++ Web_scraping_IMBD/main.py
@@ -1,12 +1,21 @@
 from bs4 import BeautifulSoup
-import request
+import requests
 import pandas as pd
 import re
 
-URL = 'http://www.imdb.com/chart/top'
+URL = 'https://letterboxd.com/jack/list/official-top-250-films-with-the-most-fans/'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Referer": ""
+}
 
 def main():
-    pass
+    response = requests.get(URL)
+    soap = BeautifulSoup(response.text, 'html.parser')
+    movies = soap.find_all('div', class_='film-poster')
+    movie_titles = [movie['data-target-link'].split('/')[2].replace('-', ' ').title() for movie in movies]
+
+
 
 if __name__ == '__main__':
     main()
