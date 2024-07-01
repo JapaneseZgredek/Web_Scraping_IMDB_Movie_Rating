c8cc0289273889fc23ffa9eb3796ba2547536db8
JapaneseZgredek <fabian.zbranski@gmail.com>
2024-07-01 20:21:44 +0200

    Import libraries

---
+++ Web_scraping_IMBD/main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import request
+import pandas as pd
+import re
+
+URL = 'http://www.imdb.com/chart/top'
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
