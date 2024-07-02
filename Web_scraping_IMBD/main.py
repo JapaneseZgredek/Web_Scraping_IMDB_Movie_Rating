7d99258065e35fd021cfb036c67d76527165255e
JapaneseZgredek <fabian.zbranski@gmail.com>
2024-07-02 20:38:45 +0200

    Get years released, directors, descriptions, ratings

--- Web_scraping_IMBD/main.py
+++ Web_scraping_IMBD/main.py
@@ -4,18 +4,34 @@ import pandas as pd
 import re
 
 URL = 'https://letterboxd.com/jack/list/official-top-250-films-with-the-most-fans/'
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Referer": ""
-}
+BASE_URL = 'https://letterboxd.com/'
+
+
+def get_years_released_directors_descriptions_ratings(links: list[str]) -> tuple[list[str], list[str], list[str], list[str]]:
+    years_released = []
+    directors = []
+    descriptions = []
+    ratings = []
+    for link in links:
+        response = requests.get(BASE_URL + link)
+        soap = BeautifulSoup(response.text, 'html.parser')
+        title_movie_dedicated_web = soap.find('title')
+        ratings.append(soap.find('meta', attrs={'name': 'twitter:data2'})['content'][:4])
+        descriptions.append(soap.find('meta', attrs={'name': 'twitter:description'})['content'])
+        years_released.append(title_movie_dedicated_web.text.split('(')[1].split(')')[0])
+        directors.append(title_movie_dedicated_web.text.split('by')[1].split('•')[0].strip())
+    return years_released, directors, descriptions, ratings
+
 
 def main():
     response = requests.get(URL)
     soap = BeautifulSoup(response.text, 'html.parser')
     movies = soap.find_all('div', class_='film-poster')
     movie_titles = [movie['data-target-link'].split('/')[2].replace('-', ' ').title() for movie in movies]
-
-
+    links = [movie['data-target-link'] for movie in movies]
+    years_released, directors, descriptions, ratings = get_years_released_directors_descriptions_ratings(links=links)
+    print(ratings)
+    print(descriptions)
 
 if __name__ == '__main__':
     main()
